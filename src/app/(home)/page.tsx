72f9fdd6275d4b215ea0c90109ebb25464d68abf
Guilherme de Carvalho Ferreira <64448040+GuiPlayer321@users.noreply.github.com>
2023-10-21 18:06:41 -0300

    feat: add first discount banner home page

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,17 @@
+"use client"
+import { useSession } from 'next-auth/react'
+import Image from 'next/image'
+import banner1 from '@/assets/banner_home_1.png'
+import banner2 from '@/assets/banner_home_2.png'
+import banner3 from '@/assets/banner_home_3.png'
+
+
+
+export default function Home() {
+  const {data} =useSession();
+  return <div className='p-5'>
+  <Image src={banner1} width={0} height={0} alt='Até 55% de desconto esse mês'
+  className='h-auto w-full'
+  />
+  </div>
+}
