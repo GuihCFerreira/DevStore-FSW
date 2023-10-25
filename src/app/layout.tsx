c60a5eceb77df2d6d45df8296fbac0ea36a72b6d
Guilherme de Carvalho Ferreira <64448040+GuiPlayer321@users.noreply.github.com>
2023-10-24 23:51:34 -0300

    style: ensure footer is always on the bottom

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,12 +20,16 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-          <AuthProvider>
-            <Header/>
-            {children}
-            <Footer/>
-          </AuthProvider>
-        </body>
+        <div className='flex flex-col h-full'>
+            <AuthProvider>
+              <Header/>
+              <div className="flex-1">
+                {children}
+              </div>
+              <Footer/>
+            </AuthProvider>
+        </div>
+      </body>
     </html>
   )
 }
