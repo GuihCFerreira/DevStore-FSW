2a45e5869ce41ae5a1b972d927b019028f514dce
Guilherme de Carvalho Ferreira <64448040+GuiPlayer321@users.noreply.github.com>
2023-10-24 21:05:18 -0300

    feat: add PromoBanner and SectionTitle components

---
+++ src/app/(home)/components/promo-banner.tsx
@@ -0,0 +1,9 @@
+import Image,{ ImageProps } from "next/image";
+
+const PromoBanner = (props :ImageProps) => {
+    return ( 
+        <Image width={0} height={0} className='h-auto w-full px-5 mt-2' {...props}/>
+     );
+}
+ 
+export default PromoBanner;
