97228820291699aa256f670a3e8defdb4b7d57af
Guilherme Ferreira <161089486+GuihCFerreira@users.noreply.github.com>
2024-08-22 09:28:40 -0300

    Update cart.tsx

--- src/components/ui/cart.tsx
+++ src/components/ui/cart.tsx
@@ -9,13 +9,22 @@ import { ScrollArea } from "./scroll-area";
 import { Button } from "./button";
 import { createCheckout } from "@/actions/checkout";
 import { loadStripe } from "@stripe/stripe-js";
+import { createOrder } from "@/actions/order";
+import { useSession } from "next-auth/react";
 
 const Cart = () => {
 
     const {products, subtotal, total, totalDiscount} = useContext(CartContext)
+    const { data } = useSession();
 
     const handleFinishPurchaseClick = async()=>{
-        const checkout = await createCheckout(products);
+
+        if (!data?.user) {
+          // TODO: redirecionar para o login
+          return;
+        }
+        const order = await createOrder(products, (data?.user as any).id);
+        const checkout = await createCheckout(products, order.id);
         const stripe = await loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY)
         stripe?.redirectToCheckout({sessionId: checkout.id})
     }
