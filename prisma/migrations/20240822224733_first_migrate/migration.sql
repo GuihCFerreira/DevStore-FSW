f404b04ff258ef8e7c89dc1faa1f5c1beee29323
GuihCFerreira <guihhh231@gmail.com>
2024-08-22 20:17:41 -0300

    setup: add new database configs

--- prisma/migrations/20240822224733_first_migrate/migration.sql
+++ prisma/migrations/20240822224733_first_migrate/migration.sql
@@ -1,3 +1,6 @@
+-- CreateEnum
+CREATE TYPE "OrderStatus" AS ENUM ('WAITING_FOR_PAYMENT', 'PAYMENT_CONFIRMED');
+
 -- CreateTable
 CREATE TABLE "Category" (
     "id" TEXT NOT NULL,
@@ -16,9 +19,6 @@ CREATE TABLE "Product" (
     "description" TEXT NOT NULL,
     "basePrice" DECIMAL(8,2) NOT NULL,
     "imageUrls" TEXT[],
-    "imageUrl1" TEXT NOT NULL,
-    "imageUrl2" TEXT NOT NULL,
-    "imageUrl3" TEXT NOT NULL,
     "categoryId" TEXT NOT NULL,
     "discountPercentage" INTEGER NOT NULL DEFAULT 0,
 
@@ -41,6 +41,29 @@ CREATE TABLE "Account" (
     "session_state" TEXT,
 
     CONSTRAINT "Account_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Order" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "status" "OrderStatus" NOT NULL DEFAULT 'WAITING_FOR_PAYMENT',
+
+    CONSTRAINT "Order_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "OrderProduct" (
+    "id" TEXT NOT NULL,
+    "productId" TEXT NOT NULL,
+    "orderId" TEXT NOT NULL,
+    "basePrice" DECIMAL(8,2) NOT NULL,
+    "discountPercentage" INTEGER NOT NULL DEFAULT 0,
+    "quantity" INTEGER NOT NULL,
+
+    CONSTRAINT "OrderProduct_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
@@ -91,6 +114,15 @@ ALTER TABLE "Product" ADD CONSTRAINT "Product_categoryId_fkey" FOREIGN KEY ("cat
 
 -- AddForeignKey
 ALTER TABLE "Account" ADD CONSTRAINT "Account_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Order" ADD CONSTRAINT "Order_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "OrderProduct" ADD CONSTRAINT "OrderProduct_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Product"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "OrderProduct" ADD CONSTRAINT "OrderProduct_orderId_fkey" FOREIGN KEY ("orderId") REFERENCES "Order"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Session" ADD CONSTRAINT "Session_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
--- src/components/ui/cart.tsx
+++ src/components/ui/cart.tsx
@@ -1,85 +1,97 @@
-import { ShoppingCartIcon } from "lucide-react";
-import { Badge } from "./badge";
-import { useContext } from "react";
-import { CartContext } from "@/providers/cart";
-import CartItem from "./cart-item";
-import { computeProductTotalPrice } from "@/helpers/product";
-import { Separator } from "@/components/ui/separator";
-import { ScrollArea } from "./scroll-area";
-import { Button } from "./button";
 import { createCheckout } from "@/actions/checkout";
-import { loadStripe } from "@stripe/stripe-js";
 import { createOrder } from "@/actions/order";
+import { Separator } from "@/components/ui/separator";
+import { computeProductTotalPrice } from "@/helpers/product";
+import { CartContext } from "@/providers/cart";
+import { loadStripe } from "@stripe/stripe-js";
+import { ShoppingCartIcon } from "lucide-react";
 import { useSession } from "next-auth/react";
+import { useContext } from "react";
+import { Badge } from "./badge";
+import { Button } from "./button";
+import CartItem from "./cart-item";
+import { ScrollArea } from "./scroll-area";
 
 const Cart = () => {
+  const { products, subtotal, total, totalDiscount } = useContext(CartContext);
+  const { data } = useSession();
 
-    const {products, subtotal, total, totalDiscount} = useContext(CartContext)
-    const { data } = useSession();
-
-    const handleFinishPurchaseClick = async()=>{
-
-        if (!data?.user) {
-          // TODO: redirecionar para o login
-          return;
-        }
-        const order = await createOrder(products, (data?.user as any).id);
-        const checkout = await createCheckout(products, order.id);
-        const stripe = await loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY)
-        stripe?.redirectToCheckout({sessionId: checkout.id})
+  const handleFinishPurchaseClick = async () => {
+    if (!data?.user) {
+      // TODO: redirecionar para o login
+      return;
     }
+    const order = await createOrder(products, (data?.user as any).id);
+    const checkout = await createCheckout(products, order.id);
+    const stripe = await loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY);
+    stripe?.redirectToCheckout({ sessionId: checkout.id });
+  };
 
-    return ( 
-        <div className="flex flex-col gap-5 h-full">
-            <Badge className="gap-1 text-base uppercase border-primary px-3 py-[0.375rem] border-2 w-fit" variant={'outline'}>
-                <ShoppingCartIcon size={16}/>
-                Carrinho
-            </Badge>
+  return (
+    <div className="flex h-full flex-col gap-5">
+      <Badge
+        className="w-fit gap-1 border-2 border-primary px-3 py-[0.375rem] text-base uppercase"
+        variant={"outline"}
+      >
+        <ShoppingCartIcon size={16} />
+        Carrinho
+      </Badge>
 
-            <div className="flex flex-col gap-5 h-full max-h-full overflow-hidden">
-                <ScrollArea className="h-full">
-                    <div className="flex flex-col h-full gap-6">
-                        {products.length > 0 ? (products.map((product) => <CartItem key={product.id} product={computeProductTotalPrice(product as any) as any} />)) : (
-                        <p className="text-center font-semibold">Carrinho vazio. </p>
-                        )}
-                    </div>
-                </ScrollArea>
-            </div>
-            
-            {products.length > 0 && (
-                <div className="flex flex-col gap-3">
-                <Separator/>
-                
-                <div className="flex items-center justify-between text-xs">
-                    <p>Subtotal</p>
-                    <p>R${subtotal.toFixed(2)}</p>
-                </div>
-
-                <Separator/>
-                <div className="flex items-center justify-between text-xs">
-                    <p>Entrega</p>
-                    <p>GRÁTIS</p>
-                </div>
-                
-                <Separator/>
-                <div className="flex items-center justify-between text-xs">
-                    <p>Descontos</p>
-                    <p>- R${totalDiscount.toFixed(2)}</p>
-                </div>
-
-                <Separator/>
-                <div className="flex items-center justify-between text-sm font-bold">
-                    <p>Total</p>
-                    <p>R${total.toFixed(2)}</p>
-                </div>
-                
-                <Button className="font-bold uppercase mt-7" onClick={handleFinishPurchaseClick}> finalizar compra</Button>
-            </div>
-
+      <div className="flex h-full max-h-full flex-col gap-5 overflow-hidden">
+        <ScrollArea className="h-full">
+          <div className="flex h-full flex-col gap-6">
+            {products.length > 0 ? (
+              products.map((product) => (
+                <CartItem
+                  key={product.id}
+                  product={computeProductTotalPrice(product as any) as any}
+                />
+              ))
+            ) : (
+              <p className="text-center font-semibold">Carrinho vazio. </p>
             )}
-            
+          </div>
+        </ScrollArea>
+      </div>
+
+      {products.length > 0 && (
+        <div className="flex flex-col gap-3">
+          <Separator />
+
+          <div className="flex items-center justify-between text-xs">
+            <p>Subtotal</p>
+            <p>R${subtotal.toFixed(2)}</p>
+          </div>
+
+          <Separator />
+          <div className="flex items-center justify-between text-xs">
+            <p>Entrega</p>
+            <p>GRÁTIS</p>
+          </div>
+
+          <Separator />
+          <div className="flex items-center justify-between text-xs">
+            <p>Descontos</p>
+            <p>- R${totalDiscount.toFixed(2)}</p>
+          </div>
+
+          <Separator />
+          <div className="flex items-center justify-between text-sm font-bold">
+            <p>Total</p>
+            <p>R${total.toFixed(2)}</p>
+          </div>
+
+          <Button
+            className="mt-7 font-bold uppercase"
+            onClick={handleFinishPurchaseClick}
+          >
+            {" "}
+            finalizar compra
+          </Button>
         </div>
-     );
-}
- 
+      )}
+    </div>
+  );
+};
+
 export default Cart;
